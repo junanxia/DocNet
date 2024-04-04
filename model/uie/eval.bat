4c6ec08614711422bd969ce23c39eb9dbb602207
谭立栋 <tanld@zjwocai.com>
2024-04-04 12:44:38 +0800

    add report.txt

--- model/uie/eval.bat
+++ model/uie/eval.bat
@@ -1,1 +1,2 @@
-python evaluate.py --model_path ./output/checkpoint-800 --test_path ../../datasets/HT-UIE/ht_new/dev.txt --batch_size 16 --max_seq_len 512 --debug
+python evaluate.py --model_path ./output-mini/checkpoint-2202 --test_path ../../datasets/HT-UIE/ht_new/dev.txt --batch_size 16 --max_seq_len 512 --debug
+python evaluate.py --model_path ./output-med/checkpoint-2202 --test_path ../../datasets/HT-UIE/ht_new/dev.txt --batch_size 16 --max_seq_len 512 --debug
