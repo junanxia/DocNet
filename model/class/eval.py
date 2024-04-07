29fc2d06b96b10c78b324abe7da53340824de88c
root <junan007@163.com>
2024-04-07 16:16:07 +0800

    remove a simple error.

--- model/class/eval.py
+++ model/class/eval.py
@@ -35,12 +35,4 @@ if __name__ == "__main__":
     args = config.parse_args()
     config = config.get_config(
         args.config, overrides=args.override, show=False)
-    engine = Engine(config, mode="eval")paddleclas_root = os.getenv("PADDLECLAS_ROOT")
-if paddleclas_root is None:
-    raise Exception("'PADDLECLAS_ROOT' is empty!!!!!")
-
-# __dir__ = os.path.dirname(os.path.abspath(__file__))
-# sys.path.append(os.path.abspath(os.path.join(__dir__, '../')))
-
-sys.path.append(paddleclas_root)
-    engine.eval()
+    engine = Engine(config, mode="eval")
