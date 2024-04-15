64fdcfd2bae6da01a7e0d683bb4d72d85ca910d5
谭立栋 <tanld@zjwocai.com>
2024-04-15 09:28:18 +0800

    update batch files.

--- model/uie/eval.bat
+++ model/uie/eval.bat
@@ -1,2 +1,1 @@
-python evaluate.py --model_path ./output-mini/checkpoint-2202 --test_path ../../datasets/HT-UIE/ht_new/dev.txt --batch_size 16 --max_seq_len 512 --debug
-python evaluate.py --model_path ./output-med/checkpoint-2202 --test_path ../../datasets/HT-UIE/ht_new/dev.txt --batch_size 16 --max_seq_len 512 --debug
+python evaluate.py --model_path ./checkpoint/model_best_mini --test_path ../../datasets/HT-UIE/data_he_202403201516_80/dev.txt --batch_size 16 --max_seq_len 512 --debug
--- model/uie/train.bat
+++ model/uie/train.bat
@@ -1,3 +1,8 @@
-echo python finetune.py --device gpu --loggging_steps 10 --save_steps 100 --eval_steps 100 --seed 42 --model_name_or_path uie-base --output_dir ./output --train_path ../../datasets/HT-UIE/ht_new/train.txt --dev_path ../../datasets/HT-UIE/ht_new/dev.txt --max_seq_length 512 --per_device_eval_batch_size 16 --per_device_train_batch_size 16 --num_train_epochs 50 --learning_rate 1e-5 --do_train --do_eval --do_export --export_model_dir ./output/infer --overwrite_output_dir --disable_tqdm True --metric_for_best_model eval_f1 --load_best_model_at_end True --save_total_limit 1
-python finetune.py --device gpu --save_steps 100 --eval_steps 100 --seed 42 --model_name_or_path uie-mini --output_dir ./output-mini --train_path ../../datasets/HT-UIE/ht_new/train.txt --dev_path ../../datasets/HT-UIE/ht_new/dev.txt --max_seq_length 512 --per_device_eval_batch_size 16 --per_device_train_batch_size 16 --num_train_epochs 2 --learning_rate 1e-5 --do_train --do_eval --do_export --export_model_dir ./output/infer-min --overwrite_output_dir --disable_tqdm True --metric_for_best_model eval_f1 --load_best_model_at_end True --save_total_limit 1
+echo "Data Merged."
+
+set model_name=uie-mini
+set check_point_path=./checkpoint/model_best_mini-100
+set dataset_path=../../datasets/HT-UIE/data_merge_0412
+
+python finetune.py --device gpu --logging_steps 3000 --save_steps 3000 --eval_steps 3000 --model_name_or_path %model_name% --output_dir %check_point_path% --train_path %dataset_path%/train.txt --dev_path %dataset_path%/dev.txt --max_seq_length 512 --per_device_eval_batch_size 24 --per_device_train_batch_size 24 --num_train_epochs 100 --learning_rate 1e-5 --do_train --do_eval --do_export --export_model_dir %check_point_path% --overwrite_output_dir --disable_tqdm True --metric_for_best_model eval_f1 --load_best_model_at_end True --save_total_limit 1 --weight_decay 0.001
 
